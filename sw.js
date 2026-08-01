4ff7a1be93d5c08db8eab757d9371c47c79aa938
moinofficial302 <moinofficial302@gmail.com>
2026-08-02 00:11:52 +0530

    Update CACHE_NAME to wedding-v1.2.0

--- sw.js
+++ sw.js
@@ -11,7 +11,7 @@
 
 'use strict';
 
-const CACHE_NAME    = 'wedding-v1.1.2';
+const CACHE_NAME    = 'wedding-v1.2.0';
 const OFFLINE_PAGE  = 'offline.html';
 
 /* ── Files to pre-cache on install ──
